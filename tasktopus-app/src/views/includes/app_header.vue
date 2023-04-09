3bfe847ab14efda282fbbff4bcc287d36ceb880e
Ainebyoona Dickson <92643336+Aine-dickson@users.noreply.github.com>
2023-04-09 05:47:32 +0300

    Worked on the app header, made neccessary responsiveness and theme

--- tasktopus-app/src/views/includes/app_header.vue
+++ tasktopus-app/src/views/includes/app_header.vue
@@ -1,9 +1,9 @@
 <template>
-    <header class="flex items-center h-full justify-between px-4">
+    <header class="flex items-center h-full justify-between px-4 space-x-2">
         <div class="logo-2">Logo</div>
-        <div class="overlow-hidden rounded-lg border border-gray-200 h-8 flex w-[90%]">
+        <div class="overlow-hidden rounded-lg border border-gray-500 dark:border-gray-200 h-10 flex w-[70%] sm:w-[90%]">
             <i class="fas fa-search"></i>
-            <input type="search" class="h-full flex-grow focus:outline-none bg-transparent">
+            <input type="search" class="h-full max-w-full flex-grow focus:outline-none bg-transparent text-xl px-2">
         </div>
         <div class="noti">
             <span><i class="fas fa-bell">NOTI</i></span>
