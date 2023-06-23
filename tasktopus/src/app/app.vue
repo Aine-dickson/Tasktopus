d03a99f5a364afa9fc5bdd157688430d7e190c28
Ainebyoona Dickson <92643336+Aine-dickson@users.noreply.github.com>
2023-06-24 01:45:58 +0300

    Filled in content for ui sampling

--- tasktopus/src/app/app.vue
+++ tasktopus/src/app/app.vue
@@ -1,10 +1,10 @@
 <template>
     <div ref="app_cont" class="app-container grid h-screen bg-blue-700">
         <header class="app-header h-full bg-green-700">
-            Header content
+            <app_header/>
         </header>
-        <nav @mouseenter="navManager(true)" @mouseout="navManager(false)" class="lg_app-nav h-full hidden sm:block bg-yellow-700">
-            Nav content
+        <nav @mouseenter="navManager(true)" @mouseleave="navManager(false)" class="lg_app-nav h-full hidden sm:block bg-yellow-700 overflow-hidden">
+            <app_lg_aside :navSize="nav_size"/>
         </nav>
         <nav class="sm_app-nav h-full sm:hidden">
             Nav content
@@ -21,20 +21,30 @@
 <script>
     import { ref } from 'vue';
 
+    import app_lg_aside from './pages/includes/app_lg_aside.vue';
+    import app_header from './pages/includes/app_header.vue';
+
     export default {
+        components: {
+            app_lg_aside,
+            app_header
+        },
         setup(){
             const app_cont = ref('')
+            const nav_size = ref('small')
 
             // Function to manage hover effect on the side nav
             const navManager = (state) => {
                 if(state){
-                    app_cont.value.style.gridTemplateColumns = "15% 85%"
+                    app_cont.value.style.gridTemplateColumns = "23% 77%"
+                    nav_size.value = 'large'
                 } else {
                     app_cont.value.style.gridTemplateColumns = "5% 95%"
+                    nav_size.value = 'small'
                 }
             }
 
-            return {app_cont, navManager}
+            return {app_cont, navManager, nav_size}
         }
     }
 </script>
@@ -45,9 +55,6 @@
         grid-template-columns: 5% 95%;
         grid-template-rows: 10% 90%;
         grid-template-areas: "header header" "lg_aside main";
-    }
-    .app-container-hover {
-      grid-template-columns: 15% 85%;
     }
     .sm_app-nav {
        grid-area: sm_aside;
